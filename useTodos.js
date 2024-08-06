57b6fec2d86ada93a85c0171a787872dafe6b803
Martin Fort <martinfort13@gmail.com>
2024-08-05 22:44:06 -0300

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,46 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos') || [])
+}
+export const useTodos= (  ) => {
+    
+    const [ todos, dispatchTodo ] = useReducer( todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ))
+
+    }, [todos])
+    
+    const handleNewTodo = (todo) =>{
+       const action = {
+        type: '[TODO] Add Todo',
+        payload: todo
+       }
+       dispatchTodo ( action );
+    }
+
+    const handleDeleteTodo = ( id ) =>{
+        dispatchTodo({
+            type: '[TODO] Remove Todo',
+            payload: id
+           });
+    }
+    const handleToggleTodo = ( id ) => {
+        console.log({id});
+        dispatchTodo({
+            type: '[TODO] Toggle Todo',
+            payload: id
+           });
+    }
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done ).length,
+        handleDeleteTodo,
+        handleToggleTodo,
+        handleNewTodo
+    }
+}
